1c9b0dcc75cf05836a921bccee2a845c18c1ba02
Tanjie McMeans <tanjmcmeans01@gmail.com>
2019-02-06 13:20:23 -0700

    Add apiCalls methods and test templates

--- src/helper/apiCalls.js
+++ src/helper/apiCalls.js
@@ -1,11 +1,158 @@
-// export const fetchFilms = async () => {
-//   const url = 'https://swapi.co/api/films/';
-//   const response = await fetch(url);
-//   if (response.ok) {
-//     const films = await response.json();
-//     return films;
-//   } else {
-//     throw new Error('films not fetched');
-//   }
-// };
+// GET requests 
+export const fetchAllVenues = async () => {
+  const url = 'https://byob-concerts.herokuapp.com/api/v1/venues';
+  const response = await fetch(url);
+  if (response.ok) {
+    const venues = await response.json();
+    return venues;
+  } else {
+    throw new Error('venues not fetched');
+  }
+}
 
+export const fetchAllConcerts = async () => {
+  const url = 'https://byob-concerts.herokuapp.com/api/v1/concerts';
+  const response = await fetch(url);
+  if (response.ok) {
+    const concerts = await response.json();
+    return concerts;
+  } else {
+    throw new Error('concerts not fetched');
+  }
+}
+
+export const fetchVenue = async (venue_id) => {
+  const url = `https://byob-concerts.herokuapp.com/api/v1/venues/${venue_id}`;
+  const response = await fetch(url);
+  if (response.ok) {
+    const venue = await response.json();
+    return venue;
+  } else {
+    throw new Error('venue not fetched');
+  }
+}
+
+export const fetchConcert = async (concert_id) => {
+  const url = `https://byob-concerts.herokuapp.com/api/v1/concerts/${concert_id}`;
+  const response = await fetch(url);
+  if (response.ok) {
+    const concert = await response.json();
+    return concert;
+  } else {
+    throw new Error('concert not fetched');
+  }
+}
+
+export const fetchConcertsByVenue = async (venue_id) => {
+  const url = `https://byob-concerts.herokuapp.com/api/v1/concerts?venue=${venue_id}`;
+  const response = await fetch(url);
+  if (response.ok) {
+    const concerts = await response.json();
+    return concerts;
+  } else {
+    throw new Error('Concerts by venue not fetched');
+  }
+}
+
+//POST requests
+export const postVenue = async (venue) => {
+  const url = 'https://byob-concerts.herokuapp.com/api/v1/venues';
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ ...venue })
+  })
+  if (response.ok) {
+    const venue = await response.json();
+    return venue;
+  } else {
+    throw new Error('venue not posted');
+  }
+}
+
+export const postConcert = async (concert) => {
+  const url = 'https://byob-concerts.herokuapp.com/api/v1/concerts';
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ ...concert })
+  })
+  if (response.ok) {
+    const concert = await response.json();
+    return concert;
+  } else {
+    throw new Error('concert not posted');
+  }
+}
+
+//PUT requests
+export const updateVenue = async (venue) => {
+  const url = `https://byob-concerts.herokuapp.com/api/v1/venues/${venue.id}`;
+  const response = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ ...venue })
+  })
+  if (response.ok) {
+    const venue = await response.json();
+    return venue;
+  } else {
+    throw new Error('venue not posted');
+  }
+}
+
+export const updateConcert = async (concert) => {
+  const url = `https://byob-concerts.herokuapp.com/api/v1/concerts/${concert.id}`;
+  const response = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ ...concert })
+  })
+  if (response.ok) {
+    const concert = await response.json();
+    return concert;
+  } else {
+    throw new Error('concert not posted');
+  }
+}
+
+//DELETE requests
+export const deleteVenue = async (venue_id) => {
+  const url = `https://byob-concerts.herokuapp.com/api/v1/venues/${venue_id}`;
+  const response = fetch(url, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  if (response.ok) {
+    const venue = await response.json();
+    return venue;
+  } else {
+    throw new Error('venue not deleted')
+  }
+}
+
+export const deleteConcert = async (concert_id) => {
+  const url = `https://byob-concerts.herokuapp.com/api/v1/concerts?venue=${concert_id}`;
+  const response = fetch(url, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  if (response.ok) {
+    const concert = await response.json();
+    return concert;
+  } else {
+    throw new Error('concert not deleted')
+  }
+}
--- src/helper/apiCalls.test.js
+++ src/helper/apiCalls.test.js
@@ -1,17 +1,115 @@
-// import * as API from './apiCalls';
-// import { mockData } from './mockData';
+import * as API from './apiCalls';
 
-// describe('API', () => {
-//   const { films, people, planets, vehicles } = mockData;
-//   beforeEach(() => {});
+describe('API', () => {
 
-//   describe('fetch films', () => {
-//     it('should call fetch with the correct params', async () => {
-//       window.fetch = jest.fn().mockImplementation(() => {
-//         return Promise.resolve({
-//           json: () => {
-//             return Promise.resolve(films);
-//           },
-//           ok: true
-//    });
-// });
+  describe('GET', () => {
+    //GET all venues
+    it('/api/v1/venues should return all venues', () => {
+
+    })
+
+    it('/api/v1/venues should throw an error if venues not fetched', () => {
+
+    })
+
+    //GET all concerts
+    it('/api/v1/concerts should return all concerts', () => {
+
+    })
+
+    it('/api/v1/concerts should throw an error if concerts not fetched', () => {
+
+    })
+
+    //GET a specific venue by id
+    it('/api/v1/venues/:venue_id should return a specific venue by id', () => {
+
+    })
+
+    it('/api/v1/venues/:venue_id should throw an error if specific venue not fetched', () => {
+
+    })
+
+    //GET a specific concert by id 
+    it('/api/v1/concerts/:concert_id should return a specific concert by id', () => {
+
+    })
+
+    it('/api/v1/concerts/:concert_id should throw an error if specific concert not fetched', () => {
+
+    })
+
+    //GET all concerts in a specific venue  
+    it('/api/v1/concerts?venue=venue_id should return all concerts in a specific venue ', () => {
+
+    })
+
+    it('/api/v1/concerts?venue=venue_id should throw an error if concerts not fetched', () => {
+
+    })
+
+  })
+
+  describe('POST', () => {
+    //POST a new venue 
+    it('/api/v1/venues should create a new venue', () => {
+
+    })
+
+    it('/api/v1/venues should return an error if venue not posted', () => {
+
+    })
+
+    //POST a new concert
+    it('/api/v1/concerts should create a new concert', () => {
+
+    })
+
+    it('/api/v1/concerts should return an error if concert not posted', () => {
+
+    })
+
+  })
+
+  describe('PUT', () => {
+    //UPDATE (PUT) a venue
+    it('/api/v1/venues/:venue_id should update  specific venue data', () => {
+
+    })
+
+    it('/api/v1/venues/:venue_id should throw an error if venue not updated', () => {
+
+    })
+
+    //UPDATE (PUT) a concert
+    it('/api/v1/concerts/:concert_id should update specific concert data', () => {
+
+    })
+
+    it('/api/v1/concerts/:concert_id should throw an error if concert not updated', () => {
+
+    })
+
+  })
+
+  describe('DELETE', () => {
+    //DELETE a venue
+    it('/api/v1/venues/:venue_id should delete a venue', () => {
+
+    })
+
+    it('/api/v1/venues/:venue_id should throw an error if venue not deleted', () => {
+
+    })
+
+    //DELETE a concert
+    it('/api/v1/concerts/:concert_id should delete a concert', () => {
+
+    })
+
+    it('/api/v1/concerts/:concert_id should throw an error if concert not deleted', () => {
+
+    })
+
+  })
+})
