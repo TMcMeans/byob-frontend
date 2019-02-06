cea1b31dc6245eba0bfc36e84919af77e8a04e46
Ashton Bailey <ashtonkbailey@gmail.com>
2019-02-06 12:56:40 -0700

    Style header and body

--- src/App.js
+++ src/App.js
@@ -4,18 +4,15 @@ import Documentation from './Documentation';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
-  }
   render() {
     return (
       <div className="App">
         <div className="header">
           <h1>Concerts</h1>
-          <h2>The Concerts API</h2>
-          <Documentation />
-          <footer>{/* NAME AND GITHUBS OF ERRBODY*/}</footer>
+          <h3>The Concerts API</h3>
         </div>
+        <Documentation />
+        <footer>{/* NAME AND GITHUBS OF ERRBODY*/}</footer>
       </div>
     );
   }
