c5b4e578f7a7e30a8aa82493b87cb66028e2f18f
Tanjie McMeans <tanjmcmeans01@gmail.com>
2019-02-05 18:00:37 -0700

    add documentation component and structure

---
+++ src/helper/apiCalls.js
@@ -0,0 +1,11 @@
+// export const fetchFilms = async () => {
+//   const url = 'https://swapi.co/api/films/';
+//   const response = await fetch(url);
+//   if (response.ok) {
+//     const films = await response.json();
+//     return films;
+//   } else {
+//     throw new Error('films not fetched');
+//   }
+// };
+
---
+++ src/helper/apiCalls.test.js
@@ -0,0 +1,17 @@
+// import * as API from './apiCalls';
+// import { mockData } from './mockData';
+
+// describe('API', () => {
+//   const { films, people, planets, vehicles } = mockData;
+//   beforeEach(() => {});
+
+//   describe('fetch films', () => {
+//     it('should call fetch with the correct params', async () => {
+//       window.fetch = jest.fn().mockImplementation(() => {
+//         return Promise.resolve({
+//           json: () => {
+//             return Promise.resolve(films);
+//           },
+//           ok: true
+//    });
+// });
